68818f616f03f275e5b53ccce4601085093e89a7
Xerxess <969371501@qq.com>
2016-02-04 16:04:45 +0800

    上传图片浏览插件-jquery 调整

--- Web/scripts/jquery.uploadPreview.js
+++ Web/scripts/jquery.uploadPreview.js
@@ -7,6 +7,11 @@
 //imgDiv                    页面DIV的JQuery的id
 //maxSize                   图片大小最大限制(K)
 //imgType                   数组后缀名
+//noimg                     默认显示图片
+//validatorerror            格式验证错误回调
+//validatorback             格式验证回调
+//callback                  回调函数
+//errorback                 异常回调 
 //**********************图片上传预览插件*************************
 (function ($) {
     jQuery.fn.extend({
@@ -18,22 +23,25 @@
                 code3: { code: -99, info: '异常错误' }
             };
             opts = jQuery.extend({
-                width: 0,
-                height: 0,
-                imgDiv: null,
+                width: 100,
+                height: 100,
                 imgBox: null,
                 maxSize: 300,
-                noimg: '',
+                isalert: true,
+                noimg: '/images/noimg.gif',
                 imgType: ["gif", "jpeg", "jpg", "bmp", "png"],
-                validatorEnd: null,
+                validatorerror: null,
+                validatorback: null,
                 callback: function () {
                     return false;
                 },
-                error: null
+                errorback: null
             }, opts || {});
             var _this = $(this);
-            var imgDiv = opts.imgDiv;
+            var _load = false;
             var imgbox = opts.imgBox;
+            var $img = null;
+            var $fit = null;//用于滤镜处理
             var version = 7;
             var filters = imgbox[0].filters ? true : false;//ie10内核filters是舍弃了的
             var v = {//判断是否是ie
@@ -74,55 +82,29 @@
                 }
                 alert(msg);
             };
-            var autoScaling = function () {
-                if (fit) {
-                    imgbox.get(0).filters.item("DXImageTransform.Microsoft.AlphaImageLoader").sizingMethod = "image";
+            var autoScaling = function (o) {
+                var $imgshow = $img;
+                if (fit) $imgshow = $fit;
+                var box_width = opts.width, img_width = opts.width;
+                var box_height = opts.height, img_height = opts.height;
+                if (o) {
+                    img_width = o['width'];
+                    img_height = o['height'];
                 }
-                //                var box_width = imgbox.width();
-                //                var box_height = imgbox.height();
-                //                var img_width = imgDiv.width();
-                //                var img_height = imgDiv.height();
-                var box_width = opts.width;
-                var box_height = opts.height;
-                var img_width = opts.width;
-                var img_height = opts.height;
-                if (fit) {
-                    img_width = box_width;
-                    img_height = box_height;
-                }
-                if (img_width > 0 && img_height > 0) {
+                if (box_width != img_width) {
                     var rate = (opts.width / img_width < opts.height / img_height) ? opts.width / img_width : opts.height / img_height;
                     if (rate <= 1) {
                         img_width = img_width * rate;
                         img_height = img_height * rate;
-                        imgbox.width(opts.width);
-                        imgbox.height(opts.height);
-                        if (fit) {
-                            imgbox.get(0).filters.item("DXImageTransform.Microsoft.AlphaImageLoader").sizingMethod = "scale";
-                        }
-                        else {
-                            imgDiv.width(img_width);
-                            imgDiv.height(img_height);
-                        }
-
-                    } else {
-                        imgDiv.width(img_width);
-                        imgDiv.height(img_height);
                     }
-                    var left = (opts.width - imgbox.width()) * 0.5;
-                    var top = (opts.height - imgbox.height()) * 0.5;
-                    //imgbox.css({"margin-left": left, "margin-top": top});
-                    setTimeout(function () {
-                        imgDiv.show();
-                        if (fit) {
-                            //imgbox.show();
-                        }
-                    }, 100);
-                }
-                else {
-                    imgbox.get(0).filters.item("DXImageTransform.Microsoft.AlphaImageLoader").sizingMethod = "scale";
-                    imgbox.width(opts.width);
-                    imgbox.height(opts.height);
+                    $imgshow.width(img_width);
+                    $imgshow.height(img_height);
+                    var left = (opts.width - img_width) * 0.5;
+                    var top = (opts.height - img_height) * 0.5;
+                    if (fit) {
+                        $imgshow[0].filters.item("DXImageTransform.Microsoft.AlphaImageLoader").sizingMethod = "scale";
+                    }
+                    $imgshow.css({ "margin-left": left, "margin-top": top }).show();
                 }
                 if (opts.callback) {
                     opts.callback();
@@ -130,42 +112,51 @@
             };
             var createImg = function () {
                 imgbox.html('');
-                var img = $("<img />");
-                imgbox.html(img);
+                $img = $("<img style='display:none;' />");
+                imgbox.html($img);
                 if (!$.support.leadingWhitespace) {
-                    imgbox.append('<span>&nbsp;</span>');
+                    imgbox.append('<span>&nbsp;</span>');//兼容ie7垂直居中
                 }
-                imgDiv = img;
             };
+            var createfit = function () {
+                $fit = $("<div style='display:none;'></div>");
+                imgbox.html($fit);
+            }
             var init = function (clear) {
+                if (!_load) {
+                    _load = true;
+                    if (opts.width) {
+                        imgbox.css('width', opts.width);
+                    }
+                    if (opts.height) {
+                        imgbox.css('height', opts.height);
+                    }
+                }
                 if (fit) {
                     imgbox.css({ filter: "" });
                 }
-                if (opts.noimg) {
-                    createImg();
-                    imgDiv.hide().attr('src', opts.noimg);
-                    imgDiv.on('load', function () {
-                        imgDiv.show();
-                    });
-                    if (!$.support.leadingWhitespace) {
-                        imgDiv.hide().attr('src', opts.noimg);
-                    }
-                }
+                createImg();
+                $img.attr('src', opts.noimg);
+                $img.on('load', function () {
+                    $img.off('load');
+                    $img.show();
+                });
+                $img.on('error', function () {
+                    $img.off('load');
+                });
             };
             init();
             _this.change(function () {
                 if (this.value) {
                     if (!RegExp("\.(" + opts.imgType.join("|") + ")$", "i").test(this.value.toLowerCase())) {
                         myalert("图片类型必须是" + opts.imgType.join("，") + "中的一种");
-                        init();
                         _this.val('');
-                        if (opts.error) {
-                            opts.error(infocode.code1);
+                        if (opts.validatorerror) {
+                            opts.validatorerror(infocode.code1);
                         }
                         return false;
                     }
-                    var file = null;
-                    var size = 0;
+                    var file, size = 0;
                     if (this.files && this.files[0]) {
                         file = this.files[0];
                         size = file.size;
@@ -173,67 +164,55 @@
                         file = this.files.item(0);
                         size = file.fileSize;
                     }
-
                     if ((size / 1024) > opts.maxSize) {
                         myalert('图片大小不能超过' + opts.maxSize + 'K');
                         _this.val('');
-                        if (opts.error) {
-                            opts.error(infocode.code2);
+                        if (opts.errorback) {
+                            opts.errorback(infocode.code2);
                         }
                         return false;
                     }
-                    //ie7\8\9
-                    var i = this;
+                    //ie7\8\9 滤镜处理
                     if (fit) {
                         try {
                             this.select();
                             this.blur();
                             var imgSrc = document.selection.createRange().text;
-                            imgbox.html('');
-                            imgbox.css({ filter: "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)" });
-                            imgbox[0].filters.item("DXImageTransform.Microsoft.AlphaImageLoader").sizingMethod = "scale";
-                            imgbox[0].filters.item('DXImageTransform.Microsoft.AlphaImageLoader').src = imgSrc;
-                            //imgbox.hide();
-                            if (opts.validatorEnd)
-                                opts.validatorEnd();
+                            createfit();
+                            $fit.css({ filter: "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)", width: 50, height: 50 });
+                            $fit[0].filters.item("DXImageTransform.Microsoft.AlphaImageLoader").sizingMethod = "image";
+                            $fit[0].filters.item('DXImageTransform.Microsoft.AlphaImageLoader').src = imgSrc;
+                            if (opts.validatorback)
+                                opts.validatorback();
                             setTimeout(function () {
-                                autoScaling();
+                                autoScaling({ 'width': $fit.width(), 'height': $fit.height() });
                             }, 100);
                         } catch (e) {
                             myalert("无效的图片文件！");
-                            if (opts.error) {
-                                opts.error(infocode.code1);
+                            if (opts.errorback) {
+                                opts.errorback(infocode.code1);
                             }
                             return;
                         }
                     }
                     else {
                         try {
-
                             //支持html5的浏览器,比如高版本的firefox、chrome、ie10
                             if (this.files && this.files[0]) {
-                                if ((this.files[0].size / 1024) > opts.maxSize) {
-                                    myalert('图片大小不能超过' + opts.maxSize + 'K');
-                                    _this.val('');
-                                    if (opts.error) {
-                                        opts.error(infocode.code2);
-                                    }
-                                    return false;
-                                }
                                 var reader = new FileReader();
                                 reader.onload = function (e) {
-                                    imgDiv.attr('src', e.target.result);
+                                    $img.hide().attr('src', e.target.result).css({ width: 'auto', height: 'auto' });
+                                    setTimeout(function () {
+                                        autoScaling({ 'width': $img.width(), 'height': $img.height() });
+                                    }, 100);
                                 };
                                 reader.readAsDataURL(this.files[0]);
-                                if (opts.validatorEnd)
-                                    opts.validatorEnd();
-                                setTimeout(function () {
-                                    autoScaling();
-                                }, 100);
+                                if (opts.validatorback)
+                                    opts.validatorback();
                             }
                         } catch (e) {
-                            if (opts.error) {
-                                opts.error(infocode.code3);
+                            if (opts.errorback) {
+                                opts.errorback(infocode.code3);
                             }
                             myalert('系统错误，请重试！');
                         }
